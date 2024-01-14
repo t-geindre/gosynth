aeb25ca60bf33f1b164a8bc7dcfb1c5fcbe7a958
Thierry Geindre <thierry.geindre@bedrockstreaming.com>
2024-01-14 13:24:38 +0100

    Add grouped dragging

--- gui/widget/grid.go
+++ gui/widget/grid.go
@@ -12,107 +12,139 @@ type Grid struct {
 	*component.Component
 	cellW, cellH   float64
 	shiftX, shiftY float64
+	dragGroup      []component.IComponent
 }
 
 func NewGrid(cellW, cellH float64) *Grid {
-	r := &Grid{
+	g := &Grid{
 		Component: component.NewComponent(),
 		cellW:     cellW,
 		cellH:     cellH,
+		dragGroup: make([]component.IComponent, 0),
 	}
 
-	r.GetLayout().SetFill(100)
-	r.GetGraphic().AddListener(&r, graphic.DrawUpdateRequiredEvent, func(e event.IEvent) {
-		r.GetGraphic().GetImage().Clear()
+	g.GetLayout().SetFill(100)
+	g.GetGraphic().AddListener(&g, graphic.DrawUpdateRequiredEvent, func(e event.IEvent) {
+		g.GetGraphic().GetImage().Clear()
 	})
 
-	behavior.NewDraggable(r)
-	r.AddListener(&r, behavior.DragEvent, func(e event.IEvent) {
+	behavior.NewDraggable(g)
+	g.AddListener(&g, behavior.DragEvent, func(e event.IEvent) {
 		dragEvent := e.(*behavior.DragEventDetails)
-		r.Shift(float64(dragEvent.DeltaX), float64(dragEvent.DeltaY))
-		r.GetGraphic().ScheduleUpdate()
+		g.Shift(float64(dragEvent.DeltaX), float64(dragEvent.DeltaY))
+		g.GetGraphic().ScheduleUpdate()
 	})
 
-	return r
+	return g
 }
 
-func (r *Grid) Shift(x, y float64) {
-	if r.shiftX == x && r.shiftY == y {
+func (g *Grid) Shift(x, y float64) {
+	if g.shiftX == x && g.shiftY == y {
 		return
 	}
 
-	r.shiftX += x
-	r.shiftY += y
+	g.shiftX += x
+	g.shiftY += y
 
-	for _, c := range r.Component.GetChildren() {
+	for _, c := range g.Component.GetChildren() {
 		l := c.GetLayout()
 		cx, cy := l.GetPosition()
 		l.SetPosition(cx+x, cy+y)
 	}
 }
 
-func (r *Grid) Append(c component.IComponent) {
-	r.Component.Append(c)
+func (g *Grid) Append(c component.IComponent) {
+	g.Component.Append(c)
 
 	behavior.NewFocusable(c)
 	behavior.NewDraggable(c)
 
-	c.AddListener(&r, behavior.DragEvent, func(e event.IEvent) {
-		r.onChildDrag(c, e)
+	c.AddListener(&g, behavior.DragStartEvent, func(e event.IEvent) {
+		g.dragGroup = append(g.dragGroup, c)
+
+		if !ebiten.IsKeyPressed(ebiten.KeyControl) {
+			return
+		}
+
+		x, y := c.GetLayout().GetPosition()
+		w, _ := c.GetLayout().GetSize()
+		x += w
+
+		for found := true; found; {
+			found = false
+			for _, c := range g.Component.GetChildren() {
+				l := c.GetLayout()
+				cx, cy := l.GetPosition()
+				if cx == x && cy == y {
+					found = true
+					g.dragGroup = append(g.dragGroup, c)
+					w, _ := c.GetLayout().GetSize()
+					x += w
+					break
+				}
+			}
+		}
 	})
 
-	c.AddListener(&r, behavior.DragStopEvent, func(e event.IEvent) {
-		r.setComponentPosition(c)
+	c.AddListener(&g, behavior.DragEvent, g.onChildDrag)
+
+	c.AddListener(&g, behavior.DragStopEvent, func(e event.IEvent) {
+		for _, c := range g.dragGroup {
+			g.setComponentPosition(c)
+		}
+		g.dragGroup = make([]component.IComponent, 0)
 	})
 
 	x, y := ebiten.CursorPosition()
 	c.GetLayout().SetAbsolutePositioning(true)
 	c.GetLayout().SetPosition(float64(x), float64(y))
 
-	r.setComponentPosition(c)
+	g.setComponentPosition(c)
 }
 
-func (r *Grid) Remove(c component.IComponent) {
-	c.RemoveListener(&r, behavior.DragEvent)
-	c.RemoveListener(&r, behavior.DragStopEvent)
-	r.Component.Remove(c)
+func (g *Grid) Remove(c component.IComponent) {
+	c.RemoveListener(&g, behavior.DragEvent)
+	c.RemoveListener(&g, behavior.DragStopEvent)
+	g.Component.Remove(c)
 }
 
-func (r *Grid) onChildDrag(c component.IComponent, e event.IEvent) {
+func (g *Grid) onChildDrag(e event.IEvent) {
 	if !ebiten.IsKeyPressed(ebiten.KeySpace) {
 		dEv := e.(*behavior.DragEventDetails)
-		px, py := c.GetLayout().GetPosition()
-		c.GetLayout().SetPosition(float64(dEv.DeltaX)+px, float64(dEv.DeltaY)+py)
+		for _, c := range g.dragGroup {
+			px, py := c.GetLayout().GetPosition()
+			c.GetLayout().SetPosition(float64(dEv.DeltaX)+px, float64(dEv.DeltaY)+py)
+		}
 		e.StopPropagation()
-		r.GetGraphic().ScheduleUpdate()
+		g.GetGraphic().ScheduleUpdate()
 	}
 }
 
-func (r *Grid) setComponentPosition(c component.IComponent) {
+func (g *Grid) setComponentPosition(c component.IComponent) {
 	l := c.GetLayout()
 
-	shiftX := float64(int(r.shiftX) % int(r.cellW))
-	shiftY := float64(int(r.shiftY) % int(r.cellH))
+	shiftX := float64(int(g.shiftX) % int(g.cellW))
+	shiftY := float64(int(g.shiftY) % int(g.cellH))
 
 	x, y := l.GetPosition()
 	if x < 0 {
-		x -= r.cellW / 2
+		x -= g.cellW / 2
 	} else {
-		x += r.cellW / 2
+		x += g.cellW / 2
 	}
 	if y < 0 {
-		y -= r.cellH / 2
+		y -= g.cellH / 2
 	} else {
-		y += r.cellH / 2
+		y += g.cellH / 2
 	}
 
 	x -= shiftX
 	y -= shiftY
 
-	cx := float64(int(x/r.cellW))*r.cellW + shiftX
-	cy := float64(int(y/r.cellH))*r.cellH + shiftY
+	cx := float64(int(x/g.cellW))*g.cellW + shiftX
+	cy := float64(int(y/g.cellH))*g.cellH + shiftY
 
 	l.SetPosition(cx, cy)
 
-	r.GetGraphic().ScheduleUpdate()
+	g.GetGraphic().ScheduleUpdate()
 }
